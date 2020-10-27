7ea07e020941ca4c800caaf24e5ef9610d29905a
luke-berzins <lukeberzins16@gmail.com>
2020-10-27 19:20:33 +0000

    Add more conditionals

--- conditionals.js
+++ conditionals.js
@@ -1,4 +1,4 @@
-/*
+
 const raining = true;
 const cold = false;
 
@@ -10,8 +10,10 @@ if (cold) {
   console.log("Make sure you pick out a scarf!");
 }
 
-console.log("Now you're ready to go outside!");
-*/
+
+
+
+
 
 const temperature = 12;
 
@@ -25,4 +27,22 @@ else {
 }
 
 console.log("Now you're ready to go outside!");
+
+if (temperature < -40 || temperature > 40 ) {
+  console.log("Maybe going outside isn't such a great idea...");
+}
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
+
+
+
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote")
+}
 
